12cad72129bfb558119f94b50f14f7c66ad8ba98
AtharvaParanjpe <atharvapparanjpe@gmail.com>
2020-12-15 01:21:28 -0500

    Second UI update

--- app.py
+++ app.py
@@ -12,28 +12,24 @@ app.secret_key = 'development key'
 
 class Form(FlaskForm):
     kValue = TextField('Peel Value', [validators.Required('Cannot be Empty!')])
-    weightOfPath = TextField('Weight of Path')
-    numNodes = TextField('Number of nodes in path')
-    executionTime = TextField('Execution Time in seconds')
+    # weightOfPath = TextField('Weight of Path')
+    # numNodes = TextField('Number of nodes in path')
+    # executionTime = TextField('Execution Time in seconds')
 
 @app.route('/Peeling', methods =['GET', 'POST'])
 def computeShortestPath():
-    form = Form()   
+    form = Form() 
+    
     if request.method == 'POST':
-        # print('Dataset choice:' + str(request.form["Datasets"]), file=sys.stderr)
-        # print('Peel Value:' + str( form.kValue.data), file=sys.stdout)
-        if form.validate() == False:
+        if not ("kValue" in request.form) or not("Datasets" in request.form):
             return render_template('ourPage.html', form = form)
         else:
-            kValue = form.kValue.data
-            ## Enter stuff here
+            kValue = request.form["kValue"]
+        ## Enter stuff here
 
+            listOfList = [ [1,11232,1000], [2,11232,1000]]
 
-
-
-
-
-            return render_template('populatedPage.html', kValue = kValue)
+            return render_template('ourPage.html', form=form, kValue = kValue, listOfList = listOfList)
     elif request.method == 'GET':
         return render_template('ourPage.html', form = form)
     
